d24e196926b99ea95dd4de962a2c2c982d68c9da
Abdullah Ceylan <abdullah@creativouse.co.uk>
2019-01-18 17:24:30 +0000

    :art: Fix cover appearing issue on Firefox

--- src/styles/poses.js
+++ src/styles/poses.js
@@ -1,5 +1,5 @@
-const transitionDuration = 300
-const transitionDelay = 350
+const transitionDuration = 300;
+const transitionDelay = 350;
 
 export const pageFade = {
   initial: {
@@ -15,29 +15,30 @@ export const pageFade = {
     opacity: 0,
     transition: { duration: transitionDuration },
   },
-}
+};
 
 export const staggerChildren = {
   enter: { staggerChildren: 50 },
-}
+};
 
 export const delayChildren = {
   enter: { delayChildren: transitionDelay },
-}
+};
 
 export const slideUp = {
   initial: { y: '110%' },
   enter: { y: 0, transition: { duration: transitionDuration } },
-}
+};
 
 export const appear = {
-  initial: { opacity: 0, y: 10 },
+  initial: { opacity: 0, y: 10, visibility: 'hidden' },
   enter: {
     opacity: 1,
+    visibility: 'visible',
     y: 0,
     transition: { duration: transitionDuration },
   },
-}
+};
 
 export const fade = {
   initial: { opacity: 0 },
@@ -45,4 +46,4 @@ export const fade = {
     opacity: 1,
     transition: { duration: transitionDuration },
   },
-}
+};
